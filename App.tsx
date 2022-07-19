31bc0139304f5d674dd2f9b5a92d85a268499aff
Kyaw Thura <44088165+kyawthura-gg@users.noreply.github.com>
2022-07-19 13:46:20 +0630

    chore: changed to nativewind

--- App.tsx
+++ App.tsx
@@ -3,16 +3,13 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { MainNavigator } from "./src/navigators";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { TailwindProvider } from "tailwindcss-react-native";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <TailwindProvider>
-        <NavigationContainer>
-          <MainNavigator />
-        </NavigationContainer>
-      </TailwindProvider>
+      <NavigationContainer>
+        <MainNavigator />
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
