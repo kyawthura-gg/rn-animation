2804e5d8359fc26d95c0323198035f771762de84
Kyaw Thura <44088165+kyawthura-gg@users.noreply.github.com>
2022-11-15 15:31:40 +0800

    chore: update mock

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,73 +1,91 @@
 export const FRIENDS = [
   {
+    id: 1,
     name: "Sophie Brown",
     photo: "https://randomuser.me/api/portraits/women/1.jpg",
   },
   {
+    id: 2,
     name: "William Garcia",
     photo: "https://randomuser.me/api/portraits/men/1.jpg",
   },
   {
+    id: 3,
     name: "Sophie Brown",
     photo: "https://randomuser.me/api/portraits/women/1.jpg",
   },
   {
+    id: 4,
     name: "William Garcia",
     photo: "https://randomuser.me/api/portraits/men/1.jpg",
   },
   {
+    id: 5,
     name: "Sophie Brown",
     photo: "https://randomuser.me/api/portraits/women/1.jpg",
   },
   {
+    id: 6,
     name: "William Garcia",
     photo: "https://randomuser.me/api/portraits/men/1.jpg",
   },
   {
+    id: 7,
     name: "Sophie Brown",
     photo: "https://randomuser.me/api/portraits/women/1.jpg",
   },
   {
+    id: 8,
     name: "William Garcia",
     photo: "https://randomuser.me/api/portraits/men/1.jpg",
   },
   {
+    id: 9,
     name: "Sophie Brown",
     photo: "https://randomuser.me/api/portraits/women/1.jpg",
   },
   {
+    id: 10,
     name: "William Garcia",
     photo: "https://randomuser.me/api/portraits/men/1.jpg",
   },
   {
+    id: 11,
     name: "Sophie Brown",
     photo: "https://randomuser.me/api/portraits/women/1.jpg",
   },
   {
+    id: 12,
     name: "William Garcia",
     photo: "https://randomuser.me/api/portraits/men/1.jpg",
   },
   {
+    id: 13,
     name: "Sophie Brown",
     photo: "https://randomuser.me/api/portraits/women/1.jpg",
   },
   {
+    id: 14,
     name: "William Garcia",
     photo: "https://randomuser.me/api/portraits/men/1.jpg",
   },
   {
+    id: 15,
     name: "Sophie Brown",
     photo: "https://randomuser.me/api/portraits/women/1.jpg",
   },
   {
+    id: 16,
     name: "William Garcia",
     photo: "https://randomuser.me/api/portraits/men/1.jpg",
   },
   {
+    id: 17,
     name: "Sophie Brown",
     photo: "https://randomuser.me/api/portraits/women/1.jpg",
   },
   {
+    id: 18,
     name: "William Garcia",
     photo: "https://randomuser.me/api/portraits/men/1.jpg",
   },
